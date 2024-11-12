751f1c95163a4b5b883cb4d976aefd2ecc4510be
dice0155 <f.dice.1997@gmail.com>
2024-11-12 19:56:31 +0900

    fix: test text

--- test/pages/home_test.dart
+++ test/pages/home_test.dart
@@ -4,23 +4,25 @@ import 'package:flutter_test/flutter_test.dart';
 import 'package:github_search_app_riverpod/pages/home.dart';
 
 void main() {
-  testWidgets('Counter increments smoke test', (WidgetTester tester) async {
-    // テスト用のウィジェットを作成
-    await tester.pumpWidget(
-      const ProviderScope(
-        child: MaterialApp(
-          home: HomePage(),
+  group('HomePage', () {
+    testWidgets('カウンター機能テスト', (WidgetTester tester) async {
+      // テスト用のウィジェットを作成
+      await tester.pumpWidget(
+        const ProviderScope(
+          child: MaterialApp(
+            home: HomePage(),
+          ),
         ),
-      ),
-    );
+      );
 
-    expect(find.text('Counter: 0'), findsOneWidget);
-    expect(find.text('Counter: 1'), findsNothing);
+      expect(find.text('Counter: 0'), findsOneWidget);
+      expect(find.text('Counter: 1'), findsNothing);
 
-    await tester.tap(find.byIcon(Icons.add));
-    await tester.pump();
+      await tester.tap(find.byIcon(Icons.add));
+      await tester.pump();
 
-    expect(find.text('Counter: 0'), findsNothing);
-    expect(find.text('Counter: 1'), findsOneWidget);
+      expect(find.text('Counter: 0'), findsNothing);
+      expect(find.text('Counter: 1'), findsOneWidget);
+    });
   });
 }
