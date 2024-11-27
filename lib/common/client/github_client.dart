57cf0929180b5570200bccea307518fdf5e88ffc
dice0155 <f.dice.1997@gmail.com>
2024-11-27 20:23:50 +0900

    add: gitHubApiClientProvider

--- lib/common/client/github_client.dart
+++ lib/common/client/github_client.dart
@@ -1,6 +1,9 @@
 import 'dart:convert';
+import 'package:flutter_riverpod/flutter_riverpod.dart';
 import 'package:github_search_app_riverpod/common/models/repository.dart';
 import 'package:http/http.dart' as http;
+
+final gitHubApiClientProvider = Provider((ref) => GitHubApiClient());
 
 class GitHubApiClient {
   final String baseUrl = 'https://api.github.com';
